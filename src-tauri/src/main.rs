f997633ec1128e12bd07d0d657623dab83eab043
sytang9 <shanyuan2000@gmail.com>
2026-05-17 00:48:21 +0800

    fix(scaffold): wire main.rs->lib.rs, add IPC mocks, set CSP, add plugin permissions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::default().build())
-        .plugin(tauri_plugin_http::init())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    rare_lib::run();
 }
